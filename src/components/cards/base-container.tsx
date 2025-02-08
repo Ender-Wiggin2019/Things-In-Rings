5e1dbfa5fa4a7da1b6fe9ae1498e79e397e64bb4
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2025-02-08 18:52:27 +0800

    feat: change size

--- src/components/cards/base-container.tsx
+++ src/components/cards/base-container.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: Ender-Wiggin
  * @Date: 2025-02-02 12:15:02
- * @LastEditors: Oushuo Huang
- * @LastEditTime: 2025-02-05 15:21:37
+ * @LastEditors: Ender-Wiggin
+ * @LastEditTime: 2025-02-08 18:51:32
  * @Description:
  */
 import React from "react";
@@ -21,7 +21,7 @@ const withBaseContainer = <P extends object>(
 		return (
 			<div
 				className={`relative ${
-					cardSize === "sm" ? "w-40 h-40 text-sm" : "w-60 h-60 text-md"
+					cardSize === "sm" ? "w-40 h-40 text-sm" : "w-56 h-56 text-md"
 				} rounded-lg p-4 shadow-lg ${bgColor} flex items-center justify-center`}
 			>
 				<WrappedComponent {...(props as P)} />
