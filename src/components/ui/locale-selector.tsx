489d4847ee179767feed4009e697d4fbd86ebe83
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2025-02-09 01:39:16 +0800

    feat: add new cards

--- src/components/ui/locale-selector.tsx
+++ src/components/ui/locale-selector.tsx
@@ -2,7 +2,7 @@
  * @Author: Ender-Wiggin
  * @Date: 2025-02-08 18:32:10
  * @LastEditors: Ender-Wiggin
- * @LastEditTime: 2025-02-08 18:40:31
+ * @LastEditTime: 2025-02-09 01:35:46
  * @Description:
  */
 
@@ -16,18 +16,17 @@ type Props = {
 };
 export default function LocaleSwitcher({ locale, locales, route }: Props) {
 	const t = useTranslations("LocaleSwitcher");
-	const otherLocales = locales?.filter((cur) => cur !== locale);
-
+	// const otherLocales = locales?.filter((cur) => cur !== locale);
 	return (
-		<div className="flex gap-2 justify-start items-center">
+		<div className="flex gap-2 justify-start items-center text-sm">
 			<div className="text-white">{t('language')}:</div>
-			{otherLocales.map((l) => {
+			{locales.map((l) => {
 				return (
 					<Link
                     key={l}
 						href={route}
 						locale={l}
-						className="text-white underline hover:underline"
+						className={`${locale === l ? 'text-word border-2 border-word p-1 rounded-md' : 'text-white underline hover:underline'}`}
 					>
 						{t(l)}
 					</Link>
